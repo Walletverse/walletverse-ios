202ff581cafb2b65ceecf92ce17a87ee462e02dd
= <zhangxiaotiana@126.com>
2023-02-17 17:57:50 +0800

    getAddress

--- Walletverse/Class/Assets/Controller/VertifyMnemonicViewController.swift
+++ Walletverse/Class/Assets/Controller/VertifyMnemonicViewController.swift
@@ -92,7 +92,7 @@ class VertifyMnemonicViewController: BaseViewController {
             if let privateKey = returnData {
                 let addressParams = JSCoreParams()
                     .put(key: "chainId", value: "0x1")?
-                    .put(key: "mnemonic", value: privateKey) ?? JSCoreParams()
+                    .put(key: "privateKey", value: privateKey) ?? JSCoreParams()
                 Walletverse.getAddress(params: addressParams) { (returnData, error) in
                     
                     if let address = returnData {
